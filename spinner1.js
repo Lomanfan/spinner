d401de25179fea6a2165fbd0c07d8d30757e5797
Liuming <lfan.canada@gmail.com>
2021-04-07 02:09:04 +0000

    Leveraging \r and setTimeout for Spinner

--- spinner1.js
+++ spinner1.js
@@ -2,18 +2,21 @@ process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
 setTimeout(() => {
   process.stdout.write('\r|  ');
-}, 1500);
+}, 150);
 
 setTimeout(() => {
   process.stdout.write('\r/   ');
-}, 2000);
+}, 200);
 
 setTimeout(() => {
   process.stdout.write('\r-   ');
-}, 2500);
+}, 250);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 3000);
+  process.stdout.write('\r\\   ');
+}, 300);
 
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 350);
