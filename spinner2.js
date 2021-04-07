ff81f5e67b5252edea5f9af33ec769eaedb5af53
Liuming <lfan.canada@gmail.com>
2021-04-07 02:28:01 +0000

    refactored and removed repeated setTimeout statements

--- spinner2.js
+++ spinner2.js
@@ -2,20 +2,15 @@
 
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
-const spins = ['\r|   ', '\r/   ', '\r-   ', '\r\\  '];
+const spins = ['\r|   ', '\r/   ', '\r-   ', '\r\\  ', '\r|  '];
 
 
 let timeOut = 200;
-  for (let spin of spins) {
+
+for (let spin of spins) {
+
   setTimeout(() => {
     process.stdout.write(spin);
   }, timeOut += 200);
+
 };
-
-
-
-  // process.stdout.write('\r|   ');
-  // process.stdout.write('\r/   ');
-  // process.stdout.write('\r-   ');
-  // process.stdout.write('\r\\  ');
-
